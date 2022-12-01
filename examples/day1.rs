d60dd108efb3b8aefa5199138f58cdcd683eeaa3
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2022-11-30 21:06:52 -0800

    day 1-1

--- examples/day1.rs
+++ examples/day1.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello world");
+use std::fs::read_to_string;
+
+fn main() -> anyhow::Result<()> {
+    let input = read_to_string("./data/day1.txt")?;
+    let mut current_elf_sum = 0;
+    let mut max_elf_sum = 0;
+    for line in input.lines() {
+        if line == "" {
+            if current_elf_sum > max_elf_sum {
+                max_elf_sum = current_elf_sum
+            }
+            current_elf_sum = 0;
+            continue;
+        }
+
+        let calories = line.parse::<i32>()?;
+        current_elf_sum += calories;
+    }
+
+    println!("max: {max_elf_sum}");
+
+    Ok(())
 }

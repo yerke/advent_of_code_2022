b887d941c5d5a75284432bd04ea945c959878908
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2022-12-02 21:50:34 -0800

    day 3-2

--- examples/day3.rs
+++ examples/day3.rs
@@ -1,27 +1,43 @@
+#![feature(iter_array_chunks)]
+
 use std::collections::HashSet;
 use std::fs::read_to_string;
 
 fn main() -> anyhow::Result<()> {
     let input = read_to_string("./data/day3.txt")?;
 
+    // part 1
     let mut part1_total = 0;
     input
         .split("\n")
         .for_each(|l| {
             let (left, right) = l.split_at(l.len() / 2);
-            dbg!(left);
-            dbg!(right);
             let left_set: HashSet<char> = left.chars().collect();
             let right_set: HashSet<char> = right.chars().collect();
             let intersection: HashSet<&char> = left_set.intersection(&right_set).collect();
-            dbg!(&intersection);
             let score = get_score(intersection);
-            dbg!(score);
             part1_total += score;
         }
         );
 
+    // part 2
+    let chunks = input
+        .split("\n")
+        .array_chunks::<3>();
+
+    let mut part2_total = 0;
+    for chunk in chunks {
+        let a_set: HashSet<char> = chunk[0].chars().collect();
+        let b_set: HashSet<char> = chunk[1].chars().collect();
+        let c_set: HashSet<char> = chunk[2].chars().collect();
+        let intersection: HashSet<char> = a_set.intersection(&b_set).copied().collect();
+        let intersection: HashSet<&char> = intersection.intersection(&c_set).collect();
+        let score = get_score(intersection);
+        part2_total += score;
+    }
+
     println!("{part1_total}");
+    println!("{part2_total}");
 
     Ok(())
 }

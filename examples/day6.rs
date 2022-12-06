0e9f184d7383f442152779c44562088c0828634e
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2022-12-05 22:00:09 -0800

    day 6-1

---
+++ examples/day6.rs
@@ -0,0 +1,16 @@
+use std::collections::HashSet;
+use std::fs::read_to_string;
+
+fn main() -> anyhow::Result<()> {
+    let input = read_to_string("./data/day6.txt")?;
+
+    for (i, window) in input.as_bytes().windows(4).enumerate() {
+        let set: HashSet<&u8> = HashSet::from_iter(window);
+        if set.len() == 4 {
+            println!("part 1: {}", i + 4);
+            break;
+        }
+    }
+
+    Ok(())
+}
